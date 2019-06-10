4fad4a7300250c62332b13c5f4901452dd333938
Oka <tonkotsu6218@gmail.com>
2019-06-11 04:30:18 +0900

    update clipboard

--- dist/js/smile-script.js
+++ dist/js/smile-script.js
@@ -1,14 +1,20 @@
 window.onload = function(){
   const conversion_button = document.getElementById("Conversion-button");
-  const tweet_button = document.getElementById("Tweet-button")
+  const tweet_button = document.getElementById("Tweet-button");
+  const copy_button = document.getElementById("Copy-to-clipboard-button");
+  const input_text = document.getElementById("Input-textarea");
+  const output_text = document.getElementById("Output-textarea");
 
   conversion_button.onclick = function(){
-    const input_text = document.getElementById("Input-textarea");
-    const output_text = document.getElementById("Output-textarea");
-
     output_text.textContent = input_text.value.replace(/\n/g, "‪♪(๑ᴖ◡ᴖ๑)♪\n") + "‪♪(๑ᴖ◡ᴖ๑)♪";
-    
+
     const in_href_text = encodeURI(output_text.value.replace(/\n/g, " ")).replace(/%E2%80%AA/g, ""); //ツイートできる形にする
     tweet_button.href = "https://twitter.com/intent/tweet?text=" + in_href_text;
+  }
+
+  copy_button.onclick = function(){
+    output_text.select();
+    document.execCommand("copy");
+    alert("コピーしました‪♪(๑ᴖ◡ᴖ๑)♪");
   }
 }
