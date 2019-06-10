5239ae241ab002336c71cb1db5cff3d955d6a359
Oka <tonkotsu6218@gmail.com>
2019-06-11 02:48:30 +0900

    fix

--- dist/js/smile-script.js
+++ dist/js/smile-script.js
@@ -1,11 +1,13 @@
 window.onload = function(){
   const conversion_button = document.getElementById("Conversion-button");
   const tweet_button = document.getElementById("Tweet-button")
+
   conversion_button.onclick = function(){
     const input_text = document.getElementById("Input-textarea");
     const output_text = document.getElementById("Output-textarea");
+
     output_text.textContent = input_text.value.replace(/\n/g, "‪♪(๑ᴖ◡ᴖ๑)♪\n") + "‪♪(๑ᴖ◡ᴖ๑)♪";
-    console.log(output_text.value);
+    
     const in_href_text = encodeURI(output_text.value.replace(/\n/g, " ")).replace(/%E2%80%AA/g, ""); //ツイートできる形にする
     tweet_button.href = "https://twitter.com/intent/tweet?text=" + in_href_text;
   }
