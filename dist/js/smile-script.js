a05aae8c16e903d1416fa89c80076e6dfb3afe99
Oka <tonkotsu6218@gmail.com>
2019-06-19 18:33:22 +0900

    [update] change constant name

--- dist/js/smile-script.js
+++ dist/js/smile-script.js
@@ -1,15 +1,15 @@
 window.addEventListener("DOMContentLoaded", function () {
-  const conversion_button = document.getElementById("Conversion-button");
-  const tweet_button = document.getElementById("Tweet-button");
-  const copy_button = document.getElementById("Copy-to-clipboard-button");
-  const input_text = document.getElementById("Input-textarea");
-  const output_text = document.getElementById("Output-textarea");
-  const main_article = document.getElementsByClassName("main-article");
+  const conversionButton = document.getElementById("Conversion-button");
+  const tweetButton = document.getElementById("Tweet-button");
+  const copyButton = document.getElementById("Copy-to-clipboard-button");
+  const inputText = document.getElementById("Input-textarea");
+  const outputText = document.getElementById("Output-textarea");
+  const mainArticle = document.getElementsByClassName("main-article");
   const footerHeight = 50;
   let target = null;
 
   function windowLoad() {
-    main_article[0].style.height = window.innerHeight - footerHeight + 'px';
+    mainArticle[0].style.height = window.innerHeight - footerHeight + 'px';
   }
   windowLoad();
   window.onresize = windowLoad;
@@ -18,20 +18,20 @@ window.addEventListener("DOMContentLoaded", function () {
     return navigator.userAgent.match(/ipad|iphone/i);
   }
   if (isOS()) {
-    copy_button.parentNode.removeChild(copy_button);
+    copyButton.parentNode.removeChild(copyButton);
   }
 
-  conversion_button.onclick = function () {
-    output_text.textContent = input_text.value.replace(/\n/g, "‪♪(๑ᴖ◡ᴖ๑)♪\n") + "‪♪(๑ᴖ◡ᴖ๑)♪";
+  conversionButton.onclick = function () {
+    outputText.textContent = inputText.value.replace(/\n/g, "‪♪(๑ᴖ◡ᴖ๑)♪\n") + "‪♪(๑ᴖ◡ᴖ๑)♪";
 
-    const in_href_text = encodeURI(output_text.value.replace(/\n/g, " ")).replace(/%E2%80%AA/g, ""); //ツイートできる形にする
-    tweet_button.href = "https://twitter.com/intent/tweet?text=" + in_href_text;
-    copy_button.textContent = "‪♪(๑ᴖ◡ᴖ๑)♪コピー♪(๑ᴖ◡ᴖ๑)♪";
+    const in_href_text = encodeURI(outputText.value.replace(/\n/g, " ")).replace(/%E2%80%AA/g, ""); //ツイートできる形にする
+    tweetButton.href = "https://twitter.com/intent/tweet?text=" + in_href_text;
+    copyButton.textContent = "‪♪(๑ᴖ◡ᴖ๑)♪コピー♪(๑ᴖ◡ᴖ๑)♪";
   }
 
-  copy_button.onclick = function () {
-    output_text.select();
+  copyButton.onclick = function () {
+    outputText.select();
     document.execCommand("copy");
-    copy_button.textContent = "‪♪(๑ᴖ◡ᴖ๑)♪コピーしたよ‪♪(๑ᴖ◡ᴖ๑)♪";
+    copyButton.textContent = "‪♪(๑ᴖ◡ᴖ๑)♪コピーしたよ‪♪(๑ᴖ◡ᴖ๑)♪";
   }
 });
