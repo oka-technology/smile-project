be81d2cab37215a3bfc61570373fce2bf97907e0
Oka <tonkotsu6218@gmail.com>
2019-06-20 20:58:51 +0900

    [add] task of static [clean]

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,27 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const pug = require('gulp-pug');
 
-gulp.task('sass', () => {
-  return gulp.src('./src/sass/**/*.scss')
-    .pipe(sass({
-      outputStyle: 'expanded'
-    }))
-    .pipe(gulp.dest('./dist/css'));
+gulp.task('sass', () => (
+  gulp.src('src/sass/**/*.scss')
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('dist/css'))
+));
+
+gulp.task('pug', () => (
+  gulp.src(['src/pug/**/*.pug', '!src/pug/**/_*.pug'])
+    .pipe(pug({}))
+    .pipe(gulp.dest('./dist'))
+));
+
+gulp.task('static', () => (
+  gulp.src(['src/js/**/*', 'src/img/**/*'], { base: 'src' })
+    .pipe(gulp.dest('dist'))
+));
+
+gulp.task('watch', async () => {
+  gulp.watch('src/sass/**/*.scss', gulp.task('sass'));
+  gulp.watch('src/pug/**/*.pug', gulp.task('pug'));
+  gulp.watch(['src/js/**/*', 'src/img/**/*'], gulp.task('static'));
 });
 
-gulp.task('pug', function buildHTML() {
-  return gulp.src('./src/pug/*.pug')
-    .pipe(pug({
-      // Your options in here.
-    }))
-    .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('watch', (done) => {
-  gulp.watch('./src/sass/*.scss', gulp.task('sass'));
-  gulp.watch(['./src/pug/*.pug', '!' + './src/pug/_*.pug'], gulp.task('pug'));
-  done();
-});
-
-gulp.task('default', gulp.parallel('sass', 'pug', 'watch'));
+gulp.task('default', gulp.series(gulp.parallel('sass', 'pug', 'static'), 'watch'));
