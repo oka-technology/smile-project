62d41fdac274e0d1ad0f69305244bb83f882ae00
Oka <tonkotsu6218@gmail.com>
2019-06-22 17:58:11 +0900

    [clean] changed how to write

--- src/js/smile-script.js
+++ src/js/smile-script.js
@@ -1,18 +1,9 @@
-window.addEventListener("DOMContentLoaded", function () {
-  const conversionButton = document.getElementById("Conversion-button");
-  const tweetButton = document.getElementById("Tweet-button");
-  const copyButton = document.getElementById("Copy-to-clipboard-button");
-  const inputText = document.getElementById("Input-textarea");
-  const outputText = document.getElementById("Output-textarea");
-  const mainArticle = document.getElementsByClassName("main-article");
-  const footerHeight = 50;
-  let target = null;
-
-  function windowLoad() {
-    mainArticle[0].style.height = window.innerHeight - footerHeight + 'px';
-  }
-  windowLoad();
-  window.onresize = windowLoad;
+window.addEventListener("DOMContentLoaded", () => {
+  const conversionButton = document.querySelector("#Conversion-button");
+  const tweetButton = document.querySelector("#Tweet-button");
+  const copyButton = document.querySelector("#Copy-to-clipboard-button");
+  const inputText = document.querySelector("#Input-textarea");
+  const outputText = document.querySelector("#Output-textarea");
 
   function isOS() {
     return navigator.userAgent.match(/ipad|iphone/i);
@@ -21,17 +12,17 @@ window.addEventListener("DOMContentLoaded", function () {
     copyButton.parentNode.removeChild(copyButton);
   }
 
-  conversionButton.onclick = function () {
+  conversionButton.addEventListener("click", () => {
     outputText.textContent = inputText.value.replace(/\n/g, "‪♪(๑ᴖ◡ᴖ๑)♪\n") + "‪♪(๑ᴖ◡ᴖ๑)♪";
 
     const in_href_text = encodeURI(outputText.value.replace(/\n/g, " ")).replace(/%E2%80%AA/g, ""); //ツイートできる形にする
     tweetButton.href = "https://twitter.com/intent/tweet?text=" + in_href_text;
     copyButton.textContent = "‪♪(๑ᴖ◡ᴖ๑)♪コピー♪(๑ᴖ◡ᴖ๑)♪";
-  }
+  });
 
-  copyButton.onclick = function () {
+  copyButton.addEventListener("click", () => {
     outputText.select();
     document.execCommand("copy");
     copyButton.textContent = "‪♪(๑ᴖ◡ᴖ๑)♪コピーしたよ‪♪(๑ᴖ◡ᴖ๑)♪";
-  }
+  });
 });
