089e0073ed3560408cbb7506c7ef5859f96391ba
Oka <tonkotsu6218@gmail.com>
2019-07-13 08:52:26 +0900

    see kind of OS

--- src/tsx/components/SmileAppMain/SmileAppMain.tsx
+++ src/tsx/components/SmileAppMain/SmileAppMain.tsx
@@ -6,16 +6,22 @@ import InputTextArea from './InputTextArea';
 import OutputTextarea from './OutputTextArea';
 import CopyButton from './CopyButton';
 import TweetButton from './TweetButton';
+
 import * as styles from './SmileAppMain.scss';
 
 const FOOTER_HEIGHT: number = 50;
 
-function determineHeight(): number {
+const determineHeight = (): number => {
   return window.innerHeight - FOOTER_HEIGHT;
 };
 
+const isAppleiOS = (): boolean => {
+  return /ipad|iphone/i.test(navigator.userAgent);
+}
+
 function SmileAppMain(props: any) {
   const [height, setHeight] = useState(determineHeight());
+  const [inputText,setInputText] = useState('');
 
   useEffect(() => {
     const onResize = () => setHeight(determineHeight());
@@ -24,16 +30,21 @@ function SmileAppMain(props: any) {
       window.removeEventListener("resize", onResize);
     }
   }, []);
+
+  const convert = (): void => {
+    console.log('test');
+    
+  }
   
   return (
     <main>
       <article className={styles.mainArticle} style={{height}}>
         <h1 className={styles.pageTitle}>‪♪(๑ᴖ◡ᴖ๑)♪</h1>
         <InputTextArea />
-        <ConversionButton />
+        <ConversionButton onConvert={convert}/>
         <OutputTextarea />
         <div className={styles.buttonContainer}>
-          <CopyButton />
+          {isAppleiOS()? null : <CopyButton />}
           <TweetButton />
         </div>
       </article>
