0164404d74b8ba4986b22879ca358789b0751e05
Oka <tonkotsu6218@gmail.com>
2019-07-13 08:53:40 +0900

    change tweetbutton's URL

--- src/tsx/components/SmileAppMain/TweetButton/TweetButton.tsx
+++ src/tsx/components/SmileAppMain/TweetButton/TweetButton.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import * as styles from './TweetButton.scss';
 
-class TweetButton extends React.Component {
-  render(){
-    return (
-      <a className={styles.normal} target="_blank" href="https://twitter.com/intent/tweet?text=&amp;url=https://oka.technology/smile-project/smile.html" >‪♪(๑ᴖ◡ᴖ๑) ♪ツイート♪(๑ᴖ◡ᴖ๑) ♪</a >
-    )
-  }
+const TWEET_URL_DEFAULT: string = "https://twitter.com/intent/tweet?text=https://oka.technology/smile-project+‪♪(๑ᴖ◡ᴖ๑)♪";
+
+function TweetButton() {
+  return (
+    <a className={styles.normal} target="_blank" href={encodeURI(TWEET_URL_DEFAULT).replace(/%E2%80%AA/g, "")} >‪♪(๑ᴖ◡ᴖ๑)♪ツイート♪(๑ᴖ◡ᴖ๑)♪</a>
+  );
 }
 
 export default TweetButton;
