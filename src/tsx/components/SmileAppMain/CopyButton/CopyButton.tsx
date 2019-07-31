2899a9b814ba728eeb3286335bd0640bf3ad9adb
Oka <tonkotsu6218@gmail.com>
2019-08-01 06:20:41 +0900

    cleaned

--- src/tsx/components/SmileAppMain/CopyButton/CopyButton.tsx
+++ src/tsx/components/SmileAppMain/CopyButton/CopyButton.tsx
@@ -1,32 +1,33 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { Transition } from 'react-transition-group';
 import * as styles from './CopyButton.scss';
 
 const isAppleiOS = (): boolean => {
   return /ipad|iphone/i.test(navigator.userAgent);
 }
 
-console.log(isAppleiOS());
+const copy = (outputTextArea: React.MutableRefObject<HTMLTextAreaElement>): void => {
+  if (isAppleiOS()) {
+    outputTextArea.current.readOnly = false;
+    const range = document.createRange();
+    range.selectNodeContents(outputTextArea.current);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    outputTextArea.current.readOnly = true;
+    window.alert('コピーしました‪♪(๑ᴖ◡ᴖ๑)♪');
+    console.log('ii');
 
+  } else {
+    outputTextArea.current.select();
+    document.execCommand("copy");
+    window.alert('コピーしました‪♪(๑ᴖ◡ᴖ๑)♪');
+  }
+}
 
 const CopyButton = (props: CopyButtonProps): JSX.Element => {
-  const copy = (outputTextArea: React.MutableRefObject<HTMLTextAreaElement>): void => {
-    if (isAppleiOS()) {
-      outputTextArea.current.readOnly = false;
-      const range = document.createRange();
-      range.selectNodeContents(outputTextArea.current);
-      window.getSelection().removeAllRanges();
-      window.getSelection().addRange(range);
-      document.execCommand('copy');
-      outputTextArea.current.readOnly = true;
-      window.alert('コピーしました‪♪(๑ᴖ◡ᴖ๑)♪');
-      console.log('ii');
-      
-    } else {
-      outputTextArea.current.select();
-      document.execCommand("copy");
-      window.alert('コピーしました‪♪(๑ᴖ◡ᴖ๑)♪');
-    }
-  }
+  
   return (
     <button className={styles.normal} type="button" onClick={() => {copy(props.areaToSelect)}}>‪♪(๑ᴖ◡ᴖ๑)♪コピー‪♪(๑ᴖ◡ᴖ๑)♪</button>
   )
